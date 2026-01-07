fd740c17f707e6a417e6b686e8dfba4203e56f67
kadengrumbles73 <kaden.grumbles@gmail.com>
2026-01-07 15:36:40 -0700

    Modify greeting and add name input prompts

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep 1 World!");
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"{last_name}, {first_name} {last_name}");
     }
 }
