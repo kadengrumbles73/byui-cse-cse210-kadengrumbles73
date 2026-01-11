e7c4b243399bfd919d3d200caddda3b47d845bbf
kadengrumbles73 <kaden.grumbles@gmail.com>
2026-01-10 17:28:09 -0700

    Made number guesser

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+
+        int num = random.Next(1, 101);
+
+        int guess = -1;
+        
+        int count = 0;
+
+        while(guess != num)
+        {
+            Console.WriteLine("What is your Guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if(guess > num)
+            {
+                Console.WriteLine("Lower");
+                count++;
+                Console.WriteLine($"Guess count: {count}");
+            }
+            else if(guess < num)
+            {
+                Console.WriteLine("Higher");
+                count++;
+                Console.WriteLine($"Guess count: {count}");
+            }
+            else  
+            {
+                Console.WriteLine("You guessed it!");
+                count++;
+                Console.WriteLine($"Guess count: {count}");
+            }
+        }
     }
 }
