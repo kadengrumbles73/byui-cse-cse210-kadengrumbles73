c92c2e87d4d08be795083b7d8f2e16314eae2127
kadengrumbles73 <kaden.grumbles@gmail.com>
2026-01-12 13:48:14 -0700

    finished numbers list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        string input = Console.ReadLine();
+        int answer = int.Parse(input);
+
+        while(answer != 0)
+        {
+            numbers.Add(answer);
+            Console.WriteLine("Enter Number: ");
+            answer = int.Parse(Console.ReadLine());
+        }
+        int sum = 0;
+        int count = 0;
+
+        foreach (int i in numbers)
+        {
+            sum += i;
+            count += 1;
+        }
+        float average = sum / count;
+        int max = numbers.Max();
+        Console.WriteLine($"Sum of list: {sum}");
+        Console.WriteLine($"Average of list: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
