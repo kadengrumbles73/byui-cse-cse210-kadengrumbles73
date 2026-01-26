c2d6b630307b6607096753bbd7270c524898c152
kadengrumbles73 <kaden.grumbles@gmail.com>
2026-01-26 16:57:00 -0700

    started develop 02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the Journal App!");
+        Console.WriteLine("Please select a choice: ");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.Write("What would you like to do? ");
+
+        string choice = Console.ReadLine();
+        
+        while (choice != "5")
+        {
+            if (choice == "1")
+            {
+                
+            }
+        }
     }
 }
